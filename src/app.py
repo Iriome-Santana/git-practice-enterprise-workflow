921d54d454fbc45a143bbafffcc5ba306eedfec0
Iriome <iriome2512@gmail.com>
2026-02-13 14:34:02 +0000

    feat: add user input

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 
-def main(name="World"):
+def greet(name: str) -> str:
     print(f"Hello {name}!")
+
+def main():
+    name = input("What is your name? ")
+    greet(name)
 
 if __name__ == "__main__":
     main()
